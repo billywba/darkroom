d11cb0e03e9c6d853e0a920ad7a0ee61491374af
billywba <billy.wba8@gmail.com>
2022-11-11 13:12:29 +0000

    added start link

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
       <Row>
         <Col>
           <h2>DE-LIGHT</h2>
+          <Link to="/info/1">START</Link>
         </Col>
       </Row>
     );
