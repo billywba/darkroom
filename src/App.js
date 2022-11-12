3be7e18f993ff5e3d65e689da7cb45d662920f74
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-11-12 19:59:21 +0000

    Spline test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container'
 
 import InfoFrame from './components/pages/InfoFrame';
 import Home from './components/pages/Home';
+import SplinePage from './components/pages/SplinePage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/home" element={ <Home/> } />
           <Route path="/info" element={ <InfoFrame/> } />
+          <Route path="/spline" element={ <SplinePage /> } />
         </Routes>
       </Container>
     </div>
---
+++ src/components/pages/SplinePage.js
@@ -0,0 +1,11 @@
+import Spline from "@splinetool/react-spline";
+
+function SplinePage() {
+    return (
+        <div style={{ }}>
+            <Spline scene="https://prod.spline.design/lQqNvehZUYUdCHgU/scene.splinecode" width={1800} height={900}/>
+        </div>
+    );
+}
+
+export default SplinePage;
