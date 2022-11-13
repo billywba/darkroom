dd75ae99ec4fe64acf026aa57566b2075b50fe68
billywba <billy.wba8@gmail.com>
2022-11-13 09:19:17 +0000

    rearrange layout start

--- src/components/pages/SplinePage.js
+++ src/components/pages/SplinePage.js
@@ -1,9 +1,20 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import Spline from "@splinetool/react-spline";
 
 function SplinePage() {
     return (
         <div style={{ }}>
-            <Spline scene="https://prod.spline.design/lQqNvehZUYUdCHgU/scene.splinecode" width={1800} height={900}/>
+            <Row>
+                <Spline scene="https://prod.spline.design/lQqNvehZUYUdCHgU/scene.splinecode" width={900} height={450}/>
+            </Row>
+
+            <Row style={{paddingTop: '16px'}}>
+                <Col xs={6}>
+                    <h3>Step</h3>
+                </Col>
+            </Row>
         </div>
     );
 }
